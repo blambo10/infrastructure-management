356c06b6b01c6bc4c7b95b56661b9ef1ab9a7918
Bryce Lamborne <blamb@live.com.au>
2021-08-15 15:13:35 +1000

    added code for unify script

--- dynamic_inventory_scripts/unifi.py
+++ dynamic_inventory_scripts/unifi.py
@@ -1,9 +1,19 @@
 from pyunifiwsi import UnifiClient
 import json
 
-client = UnifiClient('', '', '', 443, ssl_verify=False)
+# endpoint = '/proxy/network/api/s/default/stat/sta'
+endpoint = 'stat/device'
 
-data = client.get_devices()
+client = UnifiClient('', '', '192.168.1.9', 443, ssl_verify=False)
+# client = UnifiClient('UxEeheDA5ZN3', '6mJUNZzULmhQqyNxGuXt', '192.168.1.1', 443, ssl_verify=False)
+
+# print(dir(client.api))
+# data = client.devices()
+data = client.api(endpoint)
+# data = client.api('api/s/system')
+
+
+# api/s/default/stat/sta
 
 # devices = client.devices()
 # for device in devices:
@@ -11,3 +21,4 @@ data = client.get_devices()
 # data = json.loads(data[0])
 
 print(data)
+# print(json.dumps(data, indent=4, sort_keys=True))
